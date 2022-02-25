4b4602fbccd7c8eb49909482f95bcc8f44709a96
Kateryna-Mykhailova <katarrina.mk@gmail.com>
2022-02-25 16:36:58 +0100

    PersistGate

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-// import { myAction } from './redux/actions'
-import { addNewContact } from './redux/actions';
+// // import { myAction } from './redux/actions'
+// import { addNewContact } from './redux/actions';
+import { PersistGate } from 'redux-persist/integration/react';
 
 // console.log(store);
 // console.log(store.getState());
 // store.dispatch(myAction(5))
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
+    <Provider store={store.store}>
+      <PersistGate loading={null} persistor={store.persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,57 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  configureStore,
+  getDefaultMiddleware,
+  combineReducers,
+} from '@reduxjs/toolkit';
 // import { createAction, createReducer } from '@reduxjs/toolkit';
 // import { createStore } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import contactReducer from '../redux/reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { configure } from '@testing-library/react';
 import logger from 'redux-logger';
 // const store = createStore(reducer, composeWithDevTools());
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
 
-const middleware = [...getDefaultMiddleware(), logger];
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  logger,
+];
+
+const rootReducer = combineReducers({
+  contacts: contactReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: {
-    contacts: contactReducer,
-  },
+  reducer: persistedReducer,
   middleware,
   devTools: process.env.NODE_ENV !== 'production',
 });
-
-export default store;
+// export default () => {
+//   let store = createStore(persistedReducer)
+const persistor = persistStore(store);
+//   return { store, persistor }
+// }
+export default { store, persistor };
 // const initialState = [
 //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
 //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
